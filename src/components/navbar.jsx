f5b5999569c2ab1c2d3f98a70f9bd5a69b57b4df
GlennTatum <glennetatum@gmail.com>
2024-11-09 16:59:32 -0500

    fix navbar spacing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <div className="w-full bg-white bg-opacity-30 backdrop-blur-md shadow-md py-2 px-4 fixed top-0 flex justify-between items-center z-50">
-      <div className="flex items-center space-x-4">
-        
-        <Link to="/" className="text-xl font-semibold text-red-500"> {/* Smaller text for logo */}
-          CitySync
-        </Link>
-      </div>
-      <div className="hidden md:flex space-x-4"> {/* Reduced space between links */}
-        
+    <div className="pb-4">
+      <div className="w-full bg-white bg-opacity-30 backdrop-blur-md shadow-md py-2 px-4 top-0 flex justify-between items-center z-50">
+        <div className="flex items-center space-x-4">
+          
+          <Link to="/" className="text-xl font-semibold text-red-500"> {/* Smaller text for logo */}
+            HackRPI
+          </Link>
+        </div>
+        <div className="hidden md:flex space-x-4"> {/* Reduced space between links */}
+          
+        </div>
       </div>
     </div>
   );
